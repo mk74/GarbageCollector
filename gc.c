d4a1b35117fb1bb0bee28c82bd384fa458d4995d
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-11-26 15:24:31 +0000

    scavenging works

--- gc.c
+++ gc.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#define HEAP_SIZE  32
-//512
+#define HEAP_SIZE  30
 #define ROOTS_N 10
 
 #define NULL_DATA 0
 #define FWD_PTR 1
 #define CONST_INT_DATA 2
+#define CONST_PTR_DATA 3
 
 int heap[2*HEAP_SIZE] = {0};
 int from_hp = 0, to_hp = HEAP_SIZE;
-//scavenging
 
 int roots[ROOTS_N];
 int roots_i=0;
@@ -20,13 +19,14 @@ int evacuate(int i);
 void traverse_roots();
 void scaveneging();
 
-int add_data(int number);
+int add_int_data(int number);
 void add_root(int index);
 
 void print_heap();
 void print_roots();
 
 void test_case1();
+void test_case2();
 
 
 //------------------------------------------------------------------------------------------
@@ -65,8 +65,16 @@ void traverse_roots()
 
 void scaveneging()
 {
-	// int i;
-	// for(i=0; i<from_hp; i++)
+	int i=HEAP_SIZE;
+	while(i<to_hp){
+		if(heap[i]==CONST_PTR_DATA){
+			int fwd_ptr_value = heap[i+1];
+			if(fwd_ptr_value<HEAP_SIZE){
+				heap[i+1] = evacuate(heap[i+1]);
+			}
+		}
+		i+=2;
+	}
 };
 
 void copy_objs()
@@ -84,11 +92,18 @@ void copy_objs()
 //------------------------------------------------------------------------------------------
 
 
-int add_data(int number)
+int add_int_data(int number)
 {
 	int index = from_hp;
 	heap[from_hp++] = CONST_INT_DATA;
 	heap[from_hp++] = number;
+	return index;
+}
+
+int add_ptr_data(int ptr){
+	int index = from_hp;
+	heap[from_hp++] = CONST_PTR_DATA;
+	heap[from_hp++] = ptr;
 	return index;
 }
 
@@ -105,7 +120,7 @@ void add_root(int index){
 
 int main(void)
 {
-	test_case1();
+	test_case2();
 	printf("Before:\nfrom_space:\n");
 	print_heap(0, from_hp);
 	printf("roots:\n");
@@ -139,6 +154,9 @@ void print_heap(int i, int hn)
 			case CONST_INT_DATA:
 				printf("INTEGER: %d\n", heap[i+1]);
 				break;
+			case CONST_PTR_DATA:
+				printf("POINTER: %d\n", heap[i+1]);
+				break;
 		}
 		i+=2;
 	}
@@ -157,9 +175,17 @@ void print_roots()
 //------------------------------------------------------------------------------------------
 
 
-void test_case1()
+void test_case1() //traversing roots
 {
-	add_root(add_data(10));
-	add_root(add_data(30));
-	add_root(add_data(20));
-}
+	add_root(add_int_data(10));
+	add_root(add_int_data(30));
+	add_int_data(20);
+};
+
+void test_case2() //scavenging
+{
+	int ptr = add_int_data(10);
+	add_root(add_ptr_data(ptr));
+	add_root(add_int_data(30));
+	add_root(ptr);
+};
