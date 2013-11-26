e768f9eb0d5ef73b511eb28db6ce91499d4e7a39
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-11-26 13:36:00 +0000

    adding integers to heap and root's pointers to them

--- gc.c
+++ gc.c
@@ -1,5 +1,76 @@
-#include<stdio.h>
+#include <stdio.h>
+#define HEAP_SIZE  32
+//512
+#define ROOTS_N 10
 
-int main(void){
+#define NULL_DATA 0
+#define CONST_INT_DATA 1
+
+int heap[2*HEAP_SIZE] = {0};
+int hp=0;
+//evacuation(int *node)
+//scavenging
+
+int roots[ROOTS_N];
+int roots_i=0;
+
+void print_heap();
+void test_case1();
+
+int add_data(int number)
+{
+	int index = hp;
+	heap[hp++] = CONST_INT_DATA;
+	heap[hp++] = number;
+	return index;
+}
+
+void add_root(int index){
+	roots[roots_i]=index;
+	roots_i++;
+}
+
+void trigger_gc()
+{
+
+}
+
+int main(void)
+{
+	test_case1();
+	// trigger_gc();
+	print_heap();
 	return 1;
+}
+
+
+//------------------------------------------------------------------------------------------
+// Debugging functions
+//------------------------------------------------------------------------------------------
+
+
+void print_heap()
+{
+	int i=0;
+	while(i<HEAP_SIZE && heap[i]!= NULL_DATA){
+		switch(heap[i]){
+			case CONST_INT_DATA:
+				printf("INTEGER: %d\n", heap[i+1]);
+				break;
+		}
+		i+=2;
+	}
+}
+
+
+//------------------------------------------------------------------------------------------
+// Test cases
+//------------------------------------------------------------------------------------------
+
+
+void test_case1()
+{
+	add_root(add_data(10));
+	add_root(add_data(30));
+	add_root(add_data(20));
 }
